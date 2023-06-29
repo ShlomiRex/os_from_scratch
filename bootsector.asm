61506fbc00c2925b6f8af611d42692c2e0f09938
Shlomi <vgtvgy1@gmail.com>
2023-06-29 19:59:33 +0300

    stuff

--- bootsector.asm
+++ bootsector.asm
@@ -1,39 +1,31 @@
 [global start]
-[BITS 16]                               ;Tells the assembler that its a 16 bit code
-[ORG 0x7C00]                    ;Origin, tell the assembler that where the code will
-                                                ;be in memory after it is been loaded
+[BITS 16]
+[ORG 0x7C00]
 
+section .text
 start:
-    mov SI, String                  ;Store string pointer to SI
+    mov si, String                  ;Store string pointer to SI
     call PrintString                ;Call print string procedure
-    JMP $                                   ;Infinite loop, hang it here.
-
+    jmp $                                   ;Infinite loop, hang it here.
 
 PrintCharacter:                 ;Procedure to print character on screen
                                         ;Assume that ASCII value is in register AL
-    mov AH, 0x0E                    ;Tell BIOS that we need to print one charater on screen.
-    mov BH, 0x00                    ;Page no.
-    mov BL, 0x07                    ;Text attribute 0x07 is lightgrey font on black background
-
-    INT 0x10                                ;Call video interrupt
-    RET                                             ;Return to calling procedure
-
-
-
+    mov ah, 0x0E                    ;Tell BIOS that we need to print one charater on screen.
+    mov bh, 0x00                    ;Page no.
+    mov bl, 0x07                    ;Text attribute 0x07 is lightgrey font on black background
+    int 0x10                                ;Call video interrupt
+    ret                                             ;Return to calling procedure
 PrintString:                    ;Procedure to print string on screen
                                                 ;Assume that string starting pointer is in register SI
-
     next_character:                 ;Lable to fetch next character from string
-        mov AL, [SI]                    ;Get a byte from string and store in AL register
+        mov al, [SI]                    ;Get a byte from string and store in AL register
         inc SI                                  ;Increment SI pointer
         or AL, AL                               ;Check if value in AL is zero (end of string)
         jz exit_function                ;If end then return
         call PrintCharacter     ;Else print the character which is in AL register
-        JMP next_character              ;Fetch next character from string
+        jmp next_character              ;Fetch next character from string
         exit_function:                  ;End label
         ret                                             ;Return from procedure
-
-
 String db 'Hello World', 0              ;HelloWorld string ending with 0
 
 times 510 - ($ - $$) db 0               ;Fill the rest of sector with 0
---
+++ kernel.c
@@ -0,0 +1,14 @@
+#define WHITE_TXT 0x07 // white on black text
+
+k_main() // like main in a normal C program
+{
+	char *vidmem = (char *) 0xb8000;
+	unsigned int i=0;
+	while(i < (80*25*2))
+	{
+		vidmem[i]=' ';
+		i++;
+		vidmem[i]=WHITE_TXT;
+		i++;
+	};
+};
